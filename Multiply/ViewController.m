7fe93c2c115a798531a8af6656f0838890bcf5c6
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-05 17:36:47 -0600

    first commit

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UILabel *multiplierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UISlider *sliderValue;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *operatorSegmentControl;
+
+@property int result;
 
 @end
 
@@ -16,12 +23,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)onCalculateButtonPressed:(id)sender {
+
+    int inputValue = [self.numberTextField.text intValue];
+    int multiplierValue = [self.multiplierLabel.text intValue];
+
+    //checking operator function to find result via segmented index
+    if (self.operatorSegmentControl.selectedSegmentIndex == 0) {
+        self.result = inputValue * multiplierValue;
+    }
+    if (self.operatorSegmentControl.selectedSegmentIndex == 1) {
+        self.result = inputValue / multiplierValue;
+
+    }
+
+    //sets background color based on result
+    if (self.result >= 20) {
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+    else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+
+    //modulo practice, and working with else if
+    if (self.result % 5 == 0 && self.result % 3 == 0) {
+        self.answerLabel.text = @"fizzbuzz";
+    }
+    else if (self.result % 3 == 0) {
+        self.answerLabel.text = @"fizz";
+    }
+    else if (self.result % 5 == 0) {
+        self.answerLabel.text = @"buzz";
+    }
+    else  {
+        self.answerLabel.text = [NSString stringWithFormat:@"%i", self.result];
+    }
 }
+
+
+- (IBAction)onSliderValueChanged:(id)sender {
+    int newSliderValue = (int)self.sliderValue.value;
+    self.multiplierLabel.text = [NSString stringWithFormat:@"%i", newSliderValue];
+
+}
+
+
 
 @end
